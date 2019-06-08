00675a236573dc439b9d2cd24d10afdbfe59ff47
Lhourquin <lhourquin@gmail.com>
2019-06-08 18:02:02 +0200

    create event for display the nav bar

--- JS/components/burger.js
+++ JS/components/burger.js
@@ -1,12 +1,17 @@
 "use strict";
 
 const burger = document.getElementsByClassName("button-burger");
-const main = document.getElementsByTagName("main")[0];
 
-function OpenNavBar() {
+
+function openNavBar() {
+    const main = document.getElementsByTagName("main")[0];
+    const nav = document.getElementsByClassName("navbar")[0];
+    const header = document.getElementsByClassName("container-header")[0];
     console.log("thanks");
     
-    
+        main.classList.toggle("displayNone");
+        nav.classList.toggle("displayNone");
+        header.classList.toggle("displayNone");
 
+    }
 
-}
