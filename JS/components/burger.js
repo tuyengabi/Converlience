46bd3a1928e8dd9613d0a3d61aeabfacbf600735
Lhourquin <lhourquin@gmail.com>
2019-06-08 15:01:50 +0200

    start code for event burger

---
+++ JS/components/burger.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const burger = document.getElementsByClassName("button-burger");
+const main = document.getElementsByTagName("main")[0];
+
+function OpenNavBar() {
+    console.log("thanks");
+    
+    
+
+
+}
