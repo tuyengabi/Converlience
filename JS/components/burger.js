2bc2a73bc73d0f386ef9bfeeec17e173aa55e261
Lhourquin <lhourquin@gmail.com>
2019-06-11 23:51:18 +0200

    remove console.log

--- JS/components/burger.js
+++ JS/components/burger.js
@@ -4,7 +4,6 @@ function openNavBar() {
     const main = document.getElementsByTagName("main")[0];
     const nav = document.getElementsByClassName("navbar")[0];
     const header = document.getElementsByClassName("container-header")[0];
-    console.log("thanks");
     
         main.classList.toggle("displayNone");
         nav.classList.toggle("displayNone");
